ef34149d5b1322edb64a9d444c8fb1e953a31323
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2026-03-10 16:21:26 +0530

    Head tag added

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,11 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/admin.js"
-    ],
-    "/admin/[page]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/admin/[page].js"
     ]
   },
   "ampFirstPages": []
